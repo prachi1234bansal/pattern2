60788fc6fdd934a97b13b33299b3869a969c1730
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2019-07-10 10:25:45 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,19 @@
+class Pattern2 
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		int j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			System.out.print(1*i);
+		}
+		System.out.println(" ");
+	}
+
+		
+	
+	}
+}
